e60d03169785b29c3fa6770c6177bf5441e01ae8
Luccas Ambrosio <54081970+luccasambrosio@users.noreply.github.com>
2026-04-03 18:36:25 -0300

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isValid(String s) {
+        if(s.length() % 2 != 0) return false;
+        int l = 0, r = s.length() - 1;
+        Map<Character, Character> closeToOpen = Map.of(')', '(', '}', '{', ']', '[');
+        Stack<Character> chars = new Stack<>();
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(closeToOpen.containsValue(c)){
+                chars.push(c);
+            }else if(closeToOpen.containsKey(c)){
+                if(!chars.isEmpty() && chars.peek() == closeToOpen.get(c)){
+                    chars.pop();
+                }else{
+                    return false;
+                }
+            }
+        }
+        if(!chars.isEmpty()) return false;
+        return true;
+    }
+}
