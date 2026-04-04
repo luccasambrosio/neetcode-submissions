884204b98d6518150f013f43de248af350cef8e6
Luccas Ambrosio <54081970+luccasambrosio@users.noreply.github.com>
2026-04-04 08:00:21 -0300

    Add: car-fleet - submission-3

---
+++ Data Structures & Algorithms/car-fleet/submission-3.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int length = position.length;
+        int[][] cars = new int[length][2];
+        float maxTime = 0;
+        int fleets = 0;
+
+        for(int i = 0; i < length; i++){
+            cars[i][0] = position[i];
+            cars[i][1] = speed[i];
+        }
+
+        Arrays.sort(cars, (a, b) -> Integer.compare(a[0], b[0]));
+
+        for(int i = length - 1; i >= 0; i--){
+            float time = (float) (target - cars[i][0]) / cars[i][1];
+            if(time > maxTime){
+                maxTime = time;
+                fleets++;
+            }
+        }
+
+        return fleets;
+    }
+}
