f5dd1db98f4580e3977d2fcd6e413a66672b3e7f
Luccas Ambrosio <54081970+luccasambrosio@users.noreply.github.com>
2026-04-23 00:49:59 -0600

    Add: house-robber - submission-3

---
+++ Data Structures & Algorithms/house-robber/submission-3.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int rob(int[] nums) {
+        if(nums.length <= 1) return (nums.length == 1) ? nums[0] : 0;
+
+        int rob1 = 0, rob2 = 0;
+        for(int num : nums){
+            int temp = Math.max(num + rob1, rob2);
+            rob1 = rob2;
+            rob2 = temp;
+        }
+
+        return rob2;
+    }
+
+
+}
